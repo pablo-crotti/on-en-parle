a138b700cc2eb381b007b15b7824b3e4bd37e305
Nicolas Meuwly <nicolas.meuwly@heig-vd.ch>
2023-05-30 10:57:35 +0200

    add pages structure

---
+++ resources/js/Pages/Admin/Administration/Management/container.vue
@@ -0,0 +1,18 @@
+<script>
+    import AppLayout from '@/Layouts/AppLayoutAdmin.vue';
+    import axios from 'axios';
+
+    export default {
+        components: {
+            AppLayout
+        },
+    }
+</script>
+
+<template>
+    <AppLayout :title="'On en parle | Administration (Gestion)'">
+        <div class="management-wrapper">
+
+        </div>
+    </AppLayout>
+</template>
