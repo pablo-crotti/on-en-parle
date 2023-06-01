f516f97c1eb631292bbc4df839d9154ac4fef31d
Kevin Bourgeois <kevin.bourgeois@heig-vd.ch>
2023-06-01 10:50:49 +0200

    amélioration component message admin 1.1

--- resources/js/Pages/MyComponents/admin-message.vue
+++ resources/js/Pages/MyComponents/admin-message.vue
@@ -5,7 +5,8 @@ export default {
     props: ['message'],
     data() {
         return {
-
+            audioDuration: '00:00',
+            editing: false,
         };
     },
     methods: {
@@ -20,19 +21,65 @@ export default {
             const formattedDate = `${day}/${month}/${year}`;
 
             return formattedDate;
+        },
+        getStatusName(status) {
+            switch (status) {
+                case 5:
+                    return 'Diffusé';
+                    airedStatusColor = "#999999"
+                    break;
+                case 10:
+                    return 'Non diffusé';
+                    airedStatusColor = "#B2171B"
+                    break;
+                default:
+                    break;
+            }
+        },
+        isArchived(status) {
+
+            if (status === 10 || status === 5) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        audioLoaded(event) {
+            const audio = event.target;
+            const duration = Math.floor(audio.duration);
+            const minutes = Math.floor(duration / 60);
+            const seconds = duration % 60;
+            this.audioDuration = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        },
+        saveChanges() {
+            this.$emit('modify', this.message);
+            this.editing = false;
         }
     },
 
     setup(props) {
-
-
-
         const message = props.message;
 
-        console.log(message);
+        const setStatus = (status) => {
+            switch (status) {
+                case 5:
+                    airedStatusColor = "#999999"
+                    statusName = 'Diffusé';
+                    break;
+                case 10:
+                    airedStatusColor = "#B2171B"
+                    statusName = 'Non diffusé';
+                    break;
+                default:
+                    return '';
+            }
+        };
 
         let headerColor = "";
         let headerSymbol = "";
+        let airedStatusColor = "";
+        let statusName = "";
+        
 
         if (message.text.length > 0) {
             headerColor = '#8239DF';
@@ -46,12 +93,16 @@ export default {
         }
 
 
+        setStatus(message.status);
+
 
 
 
         return {
             headerColor,
-            headerSymbol
+            headerSymbol,
+            airedStatusColor,
+            statusName
         };
     },
 };
@@ -63,31 +114,44 @@ export default {
     <div class="message-wrapper">
 
         <div class="message-header" :style="{ backgroundColor: headerColor }">
-            <span class="symbol-header material-symbols-outlined">{{ headerSymbol }}</span>
-            <span v-if="message.audio.length > 0" class="audio-duration">00:00</span>
-            <span v-if="message.call.length > 0" class="message-caller">{{message.call[0].caller}}</span>
+            <div>
+                <span class="symbol-header material-symbols-outlined">{{ headerSymbol }}</span>
+                <span v-if="message.audio.length > 0" class="audio-duration">{{ this.audioDuration }}</span>
+                <span v-if="message.call.length > 0" class="message-caller">{{message.call[0].caller}}</span>
+            </div>
+        
+                <div v-show="isArchived(message.status)" class="aired-status" :style="{ backgroundColor: airedStatusColor}">{{statusName}}</div>
+            
         </div>
 
         <div class="message-body">
             <span class="message-date">{{ getFormattedDate(message) }}</span>
 
-            <div class="message-content">
 
-                
-                 <p class="message-text" v-if="message.text.length > 0">
-                    {{ 'dsadqsdqsdqswdqwdqdwqdqwqqddwdqdqwwqdaqdwqdqdwdqwdwdwdqw' }}
-                </p>
-
-                <div v-if="message.audio.length > 0">
-                    <audio controls>
+            <div v-if="message.audio.length > 0">
+                    <audio controls @loadedmetadata="audioLoaded">
                         <source :src="message.audio[0].audio_file" type="audio/mpeg">
                         Your browser does not support the audio element.
                     </audio>
                 </div>
+            <div class="message-content">
 
-                <div v-if="!message.call.length > 0" class="message-actions">
-                    <span class="material-symbols-outlined">edit</span>
-                    <span class="material-symbols-outlined">delete</span>
+                
+                 <p v-if="!editing" class="message-text">
+                    {{ message.content }}
+                </p>
+
+                <input v-else type="text" v-model="message.content" class="input" @keyup.enter="saveChanges"/>
+
+                
+
+                <div v-if="message.status !== 5" class="message-actions">
+                    <span class="material-symbols-outlined" @click="editing = !editing">edit</span>
+                    <span class="material-symbols-outlined" @click="$emit('delete', message)">delete</span>
+                </div>
+
+                <div v-else-if="message.status === 10">
+                    <span class="material-symbols-outlined">archive</span>
                 </div>
                 
             </div>
