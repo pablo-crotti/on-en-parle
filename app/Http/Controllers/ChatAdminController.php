9eab2400d1d732d0e22b7508ba7bb8177a312f3f
raey <raey.assefa@heig-vd.ch>
2023-06-02 10:07:44 +0200

    DragnDropComponents

--- app/Http/Controllers/ChatAdminController.php
+++ app/Http/Controllers/ChatAdminController.php
@@ -5,16 +5,53 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ChatMessage;
 use Inertia\Inertia;
+use App\Models\VoiceMessage;
 
 
 class ChatAdminController extends Controller
 {
     public function showChatRoom($id)
     {
+        $audio = [
+            [
+                'id' => 1,
+                'audio_file' => 'song1.mp3',
+                'chat_message_id' => 1,
+            ],
+            [
+                'id' => 2,
+                'audio_file' => 'song2.mp3',
+                'chat_message_id' => 1,
+            ],
+        ];
+
+        $messagesWithAudio = ChatMessage::where('chat_room_id', $id)
+        ->has('audio')
+        ->with('audio')
+        ->get()
+        ->map(function ($message) {
+            $audioFiles = $message->audio->map(function ($audio) {
+                return $audio->audio_file;
+            });
+    
+            return [
+                'id' => $message->id,
+                'content' => $message->content,
+                'nb_likes' => $message->nb_likes,
+                'status' => $message->status,
+                'chat_room_id' => $message->chat_room_id,
+                'audio_files' => $audioFiles,  // this will be an array of audio files
+            ];
+        });
+    
+        
         $messages = ChatMessage::where('chat_room_id', $id)->get();
+      //  $audio = VoiceMessage::where('chat_room_id', $id)->get();
+        error_log("dasdgfds"); // imprime le contenu de $audio dans le log d'erreurs
 
    return Inertia::render('Admin/Reception/Inbox/ContainerRaey', [
-            'initialMessages' => $messages
+            'initialMessages' => $messages,
+            'audioChatroom' => $messagesWithAudio,
         ]);    /* 
        //retourner à Affichecontenu.blade.php qui se trouve dans ./views les messages en tant que contenu
        //return view('Affichecontenu', ['contenu' => $messages]); */
