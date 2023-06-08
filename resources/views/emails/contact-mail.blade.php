859a40c6ae2e27ba6a308006ad9c1751d3779909
Kevin Bourgeois <kevin.bourgeois@heig-vd.ch>
2023-06-08 10:23:31 +0200

    mail v.1.1.1

--- resources/views/emails/contact-mail.blade.php
+++ resources/views/emails/contact-mail.blade.php
@@ -4,7 +4,7 @@
     <title>Nouveau message de contact</title>
 </head>
 <body>
-    <h2>Vous avez reçu un nouveau message de contact :</h2>
+    <h2>Vous avez reçu un noueveau message de contact :</h2>
     <p><strong>Nom :</strong> {{ $data['nom'] }}</p>
     <p><strong>Email :</strong> {{ $data['email'] }}</p>
     <p><strong>Avis :</strong> {{ $data['avis'] }}</p>
