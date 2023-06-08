bc83539eafcd5c58d308fd0fe21ea0db3eca5100
Kevin Bourgeois <kevin.bourgeois@heig-vd.ch>
2023-06-08 14:23:31 +0200

    fix import live

--- resources/js/Pages/Admin/Programs/Live/container.vue
+++ resources/js/Pages/Admin/Programs/Live/container.vue
@@ -6,7 +6,7 @@
     export default {
         components: {
             AppLayout,
-            modalConfirmation
+            //modalConfirmation
         },
         data() {
             return {
