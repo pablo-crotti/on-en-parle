02d19d1b40615f38ded807f580e6d696ff542239
Nicolas Meuwly <nicolas.meuwly@heig-vd.ch>
2023-06-09 11:46:01 +0200

    hide program selection on pages programs and users

--- resources/js/Layouts/AppLayoutAdmin.vue
+++ resources/js/Layouts/AppLayoutAdmin.vue
@@ -31,6 +31,7 @@ export default {
             upcomingPrograms: [],
             currentURL: window.location.href,
             showProgramSelection: false,
+            showProgramSelectionButton: false,
             currentProgramId: 0,
             lastProgramId: 0,
         }
@@ -59,8 +60,10 @@ export default {
         },
         getRoute(routeName){
             if(routeName === "inbox" || routeName === "archives" || routeName === "control" || routeName === "animator" || routeName === "management"){
+                this.showProgramSelectionButton = true;
                 return route(routeName, { id: this.currentProgramId });
             } else {
+                this.showProgramSelectionButton = false;
                 return route(routeName);
             }
         },
@@ -149,7 +152,7 @@ const switchToTeam = (team) => {
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <button class="selection-programs-button inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150" @click="toggleProgramSelection">
+                            <button class="selection-programs-button inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150" @click="toggleProgramSelection" v-if="showProgramSelectionButton">
                                 Séléctionner une émission
                                 <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
