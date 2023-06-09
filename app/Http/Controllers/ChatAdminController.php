14e36a26c310b1a60f2f89fa1ab5ce63b5411fcc
Nicolas Meuwly <nicolas.meuwly@heig-vd.ch>
2023-06-09 10:54:05 +0200

    fix archives page

--- app/Http/Controllers/ChatAdminController.php
+++ app/Http/Controllers/ChatAdminController.php
@@ -224,4 +224,19 @@ public function deleteMessage($id)
     return response()->json(['message' => 'Message deleted successfully']);
 }
 
+    public function getElementsByStatus($emission, $status)
+    {
+        $elements = [];
+
+        for ($i = 0; $i <= $status; $i++) {
+            $elements[$i] = ChatMessage::with(['text', 'audio', 'call'])
+                ->where('status', $i)
+                ->where('chat_room_id', $emission)
+                ->orderBy('created_at', 'DESC')
+                ->get();
+        }
+
+        return $elements;
+    }
+
 }
